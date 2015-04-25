c29670b367dea182251ca41d212e5b2c8b122a5a
Robert Crosby <Rob@Roberts-MacBook-Air.local>
2015-04-25 11:56:55 -0700

    Initial Commit

---
+++ Remote Fart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Remote Fart
+//
+//  Created by Robert Crosby on 4/25/15.
+//  Copyright (c) 2015 Robert Crosby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Remote Fart/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Remote Fart
+//
+//  Created by Robert Crosby on 4/25/15.
+//  Copyright (c) 2015 Robert Crosby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
